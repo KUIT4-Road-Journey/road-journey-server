c6518cbd76dc25868d99f11663c3bc885bde6813
cyLee1111 <flora021111@gmail.com>
2025-01-14 18:40:04 +0900

    Initial commit

---
+++ src/main/java/com/road_journey/road_journey/RoadJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.road_journey.road_journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/road_journey/road_journey/RoadJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.road_journey.road_journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
