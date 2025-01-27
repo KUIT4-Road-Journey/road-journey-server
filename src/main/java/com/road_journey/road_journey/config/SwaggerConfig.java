9ad535d97baeba647e7f472046c98765d9172a75
cyLee1111 <flora021111@gmail.com>
2025-01-27 23:12:37 +0900

    fix: swagger title

--- src/main/java/com/road_journey/road_journey/config/SwaggerConfig.java
+++ src/main/java/com/road_journey/road_journey/config/SwaggerConfig.java
@@ -16,8 +16,8 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .title("Road Journey")
+                .description("Road Journey Swagger UI")
                 .version("1.0.0");
     }
 }
