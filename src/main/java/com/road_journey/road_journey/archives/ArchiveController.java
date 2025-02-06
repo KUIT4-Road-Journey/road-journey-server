ebc29c23cc103de8cf2412a9fa4c39efff7a714b
jaehyeonchoi <asia0413@konkuk.ac.kr>
2025-02-06 15:45:05 +0900

    feat: goalId를 통해 개별 Goal(Archive) 조회

--- src/main/java/com/road_journey/road_journey/archives/ArchiveController.java
+++ src/main/java/com/road_journey/road_journey/archives/ArchiveController.java
@@ -1,5 +1,8 @@
 package com.road_journey.road_journey.archives;
 
+import com.road_journey.road_journey.goals.dto.GoalResponseDto;
+import com.road_journey.road_journey.goals.service.GoalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/archives")
 @RestController
 public class ArchiveController {
+
+    @Autowired
+    private GoalService goalService;
 
     @GetMapping("/list/{userId}")
     public String getArchiveList(@PathVariable Long userId,
@@ -24,8 +30,8 @@ public class ArchiveController {
     }
 
     @GetMapping("/{goalId}")
-    public String getArchive(@PathVariable Long goalId) {
-        return "Archive " + goalId;
+    public GoalResponseDto getArchive(@PathVariable Long goalId) {
+        return goalService.getGoalResponse(goalId);
     }
 
 }
