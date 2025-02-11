555c29cceefc4c50619d37d1ad918f6a14b771cd
cyLee1111 <flora021111@gmail.com>
2025-02-11 14:53:47 +0900

    feat: add /my api

--- src/main/java/com/road_journey/road_journey/config/SwaggerConfig.java
+++ src/main/java/com/road_journey/road_journey/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.road_journey.road_journey.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme()))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .info(apiInfo());
     }
 
@@ -19,5 +22,14 @@ public class SwaggerConfig {
                 .title("Road Journey")
                 .description("Road Journey Swagger UI")
                 .version("1.0.0");
+    }
+
+    // JWT 인증을 위한 SecurityScheme 설정
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("Authorization");
     }
 }
