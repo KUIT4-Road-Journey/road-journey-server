e5c6b323e25b64fe1f429b10454f8be56a9af396
cyLee1111 <flora021111@gmail.com>
2025-02-13 00:35:48 +0900

    fix: change userId to id

--- src/main/java/com/road_journey/road_journey/auth/domain/User.java
+++ src/main/java/com/road_journey/road_journey/auth/domain/User.java
@@ -13,7 +13,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
-    private Long userId;
+    private Long id;
 
     @Column(name = "account_id", nullable = false, unique = true)
     private String accountId;
