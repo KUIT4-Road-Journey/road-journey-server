3a25c6c81b49bb545b1dc798ae0adff3e8d6a69c
jaehyeonchoi <asia0413@konkuk.ac.kr>
2025-02-20 22:04:37 +0900

    fix: 목표 리스트 조회 api에서 출력 색상도 제공

--- src/main/java/com/road_journey/road_journey/goals/dto/GoalListResponseDto.java
+++ src/main/java/com/road_journey/road_journey/goals/dto/GoalListResponseDto.java
@@ -28,16 +28,29 @@ public class GoalListResponseDto {
         private final String progressStatus;
         private final int progress;
         private final LocalDate expireAt;
+        private final String color;
 
         GoalInfo(Goal goal) {
             this.goalId = goal.getGoalId();
             this.title = goal.getTitle();
             this.difficulty = goal.getDifficulty();
             this.isSharedGoal = goal.isSharedGoal();
-            this.sharedStatus = getSharedStatus();
+            this.sharedStatus = goal.getSharedStatus();
             this.progressStatus = goal.getProgressStatus();
             this.progress = goal.getProgress();
             this.expireAt = goal.getPeriodGoal().getExpireAt();
+            this.color = checkColor();
+        }
+
+        private String checkColor() {
+            if (progressStatus.equals("failed")) { // 실패한 목표인 경우
+                return "red";
+            }
+            if (!sharedStatus.equals("registered") || progressStatus.equals("complete-pending")) {
+                // 시작 대기 중이거나, 완료 대기 중인 경우
+                return "white";
+            }
+            return "blue";
         }
     }
 }
